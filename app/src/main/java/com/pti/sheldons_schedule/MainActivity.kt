2315da952ab06c8a639254fe6526d0cc2e034c57
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-04-28 12:02:00 +0300

    PR comments

--- app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
+++ app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.pti.sheldons_schedule.ui.theme.Sheldons_ScheduleTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
