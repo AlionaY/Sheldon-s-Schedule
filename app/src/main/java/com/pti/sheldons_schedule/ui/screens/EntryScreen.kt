fb559c92fb36a9c4dc7fda20fbb3d8620a5972ed
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-04-29 14:58:01 +0300

    Fix circle reveal animation

--- app/src/main/java/com/pti/sheldons_schedule/ui/screens/EntryScreen.kt
+++ app/src/main/java/com/pti/sheldons_schedule/ui/screens/EntryScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
@@ -33,14 +34,13 @@ fun EntryScreen(modifier: Modifier = Modifier) {
             }
         }
         val maxRadiusPx = hypot(width, height)
-        
+
         LaunchedEffect(key1 = isClicked) {
             if (isClicked) {
-                animateShape.animateTo(
-                    maxRadiusPx, animationSpec = tween()
-                ) {
+                animateShape.animateTo(maxRadiusPx, animationSpec = tween()) {
                     radius = value
                 }
+                animateShape.snapTo(0f)
             }
             isClicked = false
         }
@@ -51,7 +51,8 @@ fun EntryScreen(modifier: Modifier = Modifier) {
             .drawBehind {
                 drawCircle(
                     color = Sky,
-                    radius = radius
+                    radius = radius,
+                    center = Offset(size.width, size.height)
                 )
             })
 
