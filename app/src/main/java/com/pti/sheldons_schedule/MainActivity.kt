589535d88b23d7165ad39d9bdb7edf13974e666f
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-05-03 19:10:47 +0300

    Add firebase imports; add hilt annotations; make App file

--- app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
+++ app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
@@ -11,34 +11,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pti.sheldons_schedule.ui.theme.Sheldons_ScheduleTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Sheldons_ScheduleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Sheldons_ScheduleTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/pti/sheldons_schedule/ui/screens/EntryScreen.kt
@@ -0,0 +1,7 @@
+package com.pti.sheldons_schedule.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun EntryScreen() {
+}
