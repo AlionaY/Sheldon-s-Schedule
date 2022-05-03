30a928105ecbc6bd0334ffa788aa482265b1d886
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-05-03 19:27:45 +0300

    Fix imports; add entry screen

--- app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
+++ app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.pti.sheldons_schedule.ui.screens.EntryScreen
 import com.pti.sheldons_schedule.ui.theme.Sheldons_ScheduleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -24,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    EntryScreen()
                 }
             }
         }
--- app/src/main/java/com/pti/sheldons_schedule/ui/screens/EntryScreen.kt
+++ app/src/main/java/com/pti/sheldons_schedule/ui/screens/EntryScreen.kt
@@ -1,7 +1,67 @@
 package com.pti.sheldons_schedule.ui.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
+import com.pti.sheldons_schedule.ui.theme.Sky
+import kotlin.math.hypot
 
 @Composable
 fun EntryScreen() {
-}
+    Box(modifier = Modifier.fillMaxSize()) {
+        var radius by remember { mutableStateOf(0f) }
+        var isClicked by remember { mutableStateOf(false) }
+        val animateShape = remember { Animatable(0f) }
+        val (width, height) = with(LocalConfiguration.current) {
+            with(LocalDensity.current) {
+                screenWidthDp.dp.toPx() to screenHeightDp.dp.toPx()
+            }
+        }
+        val maxRadiusPx = hypot(width, height)
+
+        LaunchedEffect(key1 = isClicked) {
+            if (isClicked) {
+                animateShape.animateTo(maxRadiusPx, animationSpec = tween()) {
+                    radius = value
+                }
+                animateShape.snapTo(0f)
+            }
+            isClicked = false
+        }
+
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Transparent)
+            .drawBehind {
+                drawCircle(
+                    color = Sky,
+                    radius = radius,
+                    center = Offset(size.width, size.height)
+                )
+            })
+
+        FloatingActionButton(
+            onClick = { isClicked = !isClicked },
+            modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, bottom = 15.dp)
+                .align(Alignment.BottomEnd)
+        ) {
+            Icon(Icons.Filled.Add, "")
+        }
+    }}
