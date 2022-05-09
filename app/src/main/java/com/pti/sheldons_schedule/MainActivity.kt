05c4393767c1a135abfe53ccf0ca1ad7f37ff20e
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-05-09 22:56:42 +0300

    Navigation: Animate navigation to Create event screen

--- app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
+++ app/src/main/java/com/pti/sheldons_schedule/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.pti.sheldons_schedule.ui.navigation.Navigation
 import com.pti.sheldons_schedule.ui.theme.Sheldons_ScheduleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@OptIn(ExperimentalAnimationApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
@@ -26,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    val navController = rememberNavController()
+                    val navController = rememberAnimatedNavController()
                     Navigation(navHostController = navController)
                 }
             }
--- app/src/main/java/com/pti/sheldons_schedule/ui/navigation/Navigation.kt
+++ app/src/main/java/com/pti/sheldons_schedule/ui/navigation/Navigation.kt
@@ -1,16 +1,24 @@
 package com.pti.sheldons_schedule.ui.navigation
 
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
 import com.pti.sheldons_schedule.MainViewModel
 import com.pti.sheldons_schedule.ui.screens.CreateEventScreen
 import com.pti.sheldons_schedule.ui.screens.EntryScreen
 
+@ExperimentalAnimationApi
 @Composable
 fun Navigation(navHostController: NavHostController) {
 
@@ -18,8 +26,11 @@ fun Navigation(navHostController: NavHostController) {
         "No ViewModelStoreOwner was provided via LocalViewModelStoreOwner"
     }
 
-    NavHost(navController = navHostController, startDestination = NavDestination.EntryScreen.route) {
-        composable(NavDestination.EntryScreen.route) {
+    AnimatedNavHost(
+        navController = navHostController,
+        startDestination = NavDestination.EntryScreen.route
+    ) {
+        addScreen(NavDestination.EntryScreen.route) {
             CompositionLocalProvider(
                 LocalViewModelStoreOwner provides viewModelStoreOwner
             ) {
@@ -28,8 +39,36 @@ fun Navigation(navHostController: NavHostController) {
             }
         }
 
-        composable(NavDestination.CreateEventScreen.route) {
+        addScreen(NavDestination.CreateEventScreen.route) {
             CreateEventScreen()
         }
+    }
+}
+
+
+@ExperimentalAnimationApi
+fun NavGraphBuilder.addScreen(
+    route: String,
+    content: @Composable AnimatedVisibilityScope.(NavBackStackEntry) -> Unit
+) {
+
+    val enterTransition = slideInHorizontally(
+        initialOffsetX = { it },
+        animationSpec = tween(500)
+    )
+
+    val popExitTransition = slideOutHorizontally(
+        targetOffsetX = { it },
+        animationSpec = tween(500)
+    )
+
+    composable(
+        route = route,
+        enterTransition = { enterTransition },
+        exitTransition = { null },
+        popEnterTransition = { null },
+        popExitTransition = { popExitTransition }
+    ) {
+        content(it)
     }
 }
