d88bdad85a29d78177befadd0ea5811e11d130bf
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-05-12 11:57:24 +0300

    Create event screen: Date picker - Add a date signature

--- app/src/main/java/com/pti/sheldons_schedule/ui/screens/create_event_screen/CreateEventScreen.kt
+++ app/src/main/java/com/pti/sheldons_schedule/ui/screens/create_event_screen/CreateEventScreen.kt
@@ -2,16 +2,20 @@ package com.pti.sheldons_schedule.ui.screens.create_event_screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.pti.sheldons_schedule.CreateEventViewModel
 import com.pti.sheldons_schedule.R
 import com.pti.sheldons_schedule.ui.theme.LightSky
+import com.pti.sheldons_schedule.ui.theme.Steel
 
 private const val PADDING_WIDTH_SUM = 60
 private const val FIELD_COUNT = 2
@@ -52,7 +56,30 @@ fun CreateEventScreen(viewModel: CreateEventViewModel = hiltViewModel()) {
                 label = stringResource(id = R.string.description),
                 modifier = Modifier.padding(horizontal = 15.dp)
             )
-            HeightSpacer(height = 15.dp)
+            HeightSpacer(height = 18.dp)
+            Row(
+                modifier = Modifier
+                    .padding(horizontal = 15.dp)
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+            ) {
+                Text(
+                    text = stringResource(id = R.string.start_date).uppercase(),
+                    modifier = Modifier.width(halfFieldWidth.dp),
+                    fontSize = 10.sp,
+                    color = Steel,
+                    textAlign = TextAlign.Start
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Text(
+                    text = stringResource(id = R.string.end_date).uppercase(),
+                    modifier = Modifier.width(halfFieldWidth.dp),
+                    fontSize = 10.sp,
+                    color = Steel,
+                    textAlign = TextAlign.Start
+                )
+            }
+            HeightSpacer(height = 2.dp)
             Row(modifier = Modifier.fillMaxWidth()) {
                 DatePickerField(
                     pickedDate = createEventScreenState.formattedStartDate,
