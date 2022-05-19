e22327bd0d5d5c67d4527546d4e70a281e4855c6
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-05-19 18:36:04 +0300

    Main screen: WIP

---
+++ app/src/main/java/com/pti/sheldons_schedule/ui/screens/main_screen/MainScreen.kt
@@ -0,0 +1,18 @@
+package com.pti.sheldons_schedule.ui.screens.main_screen
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier.fillMaxWidth().height(58.dp)) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/pti/sheldons_schedule/ui/screens/main_screen/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.pti.sheldons_schedule.ui.screens.main_screen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+}
