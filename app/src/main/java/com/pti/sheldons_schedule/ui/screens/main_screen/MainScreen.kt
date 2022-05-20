3d4ffd1cf17fffad697aa1bc4c8bc85121eda957
aliona.yaroshenko <aliona.yaroshenko@igapgroup.com>
2022-05-20 15:02:43 +0300

    Main screen: Make header prototype

--- app/src/main/java/com/pti/sheldons_schedule/ui/screens/main_screen/MainScreen.kt
+++ app/src/main/java/com/pti/sheldons_schedule/ui/screens/main_screen/MainScreen.kt
@@ -1,18 +1,75 @@
 package com.pti.sheldons_schedule.ui.screens.main_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
+import com.pti.sheldons_schedule.ui.theme.Black
+import com.pti.sheldons_schedule.ui.theme.LightSky
 
+private const val SPACER_WIDTH = 60
+
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+    val list = listOf("M", "T", "W", "T", "F", "S", "S")
+    val pagerState = rememberPagerState()
 
     Box(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier.fillMaxWidth().height(58.dp)) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(58.dp)
+        ) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .width(SPACER_WIDTH.dp)
+            )
 
+            HorizontalPager(
+                count = list.size,
+                state = pagerState,
+                modifier = Modifier.fillMaxSize()
+            ) { page ->
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .background(LightSky)
+                ) {
+                    val config = LocalConfiguration.current
+                    val width = ((config.screenWidthDp - SPACER_WIDTH).toFloat() / 7f).toInt()
+
+                    list.forEach {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxHeight()
+                                .width(width.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Text(
+                                text = it,
+                                modifier = Modifier.wrapContentSize(),
+                                color = Black,
+                                fontSize = 15.sp,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
